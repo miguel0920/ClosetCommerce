c9eecd2bb6df01ac1b949d952e822ea803acb53d
Miguel Barahona <miguelbarahona00@gmail.com>
2022-02-12 23:00:14 -0500

    Create project Catalog.Test

---
+++ Closet.Commerce/src/Services/Catalog/Catalog.Tests/Config/ApplicationDbContextInMemory.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.Persistence.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Tests.Config
+{
+    public static class ApplicationDbContextInMemory
+    {
+        //public static ApplicationDbContext Get()
+        //{
+        //    var options = new DbContextOptionsBuilder<ApplicationDbContext>();
+        //}
+    }
+}
---
+++ Closet.Commerce/src/Services/Catalog/Catalog.Tests/ProductInStockUpdateEventHandlerTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Catalog.Tests
+{
+    [TestClass]
+    public class ProductInStockUpdateEventHandlerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
