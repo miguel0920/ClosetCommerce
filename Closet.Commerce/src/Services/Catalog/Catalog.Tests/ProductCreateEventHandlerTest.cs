bec46817f71033f6ffc3953094b98d615db2b46a
Miguel Barahona <miguelbarahona00@gmail.com>
2022-02-14 21:05:49 -0500

    Create unit tests to the crud of each table of the catalog api

---
+++ Closet.Commerce/src/Services/Catalog/Catalog.Tests/ProductCreateEventHandlerTest.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.Service.EventHandlers;
+using Catalog.Service.EventHandlers.Commands;
+using Catalog.Tests.Config;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+
+namespace Catalog.Test
+{
+    [TestClass]
+    public class ProductCreateEventHandlerTest
+    {
+        [TestMethod]
+        public void TryToProductCreate()
+        {
+            var context = ApplicationDbContextInMemory.Get();
+
+            var handler = new ProductCreateEventHandler(context);
+            handler.Handle(new ProductCreateCommand
+            {
+                Description = "Table",
+                Name = "Table",
+                Price = 18000
+            }, new CancellationToken()).Wait();
+        }
+    }
+}
--- Closet.Commerce/src/Services/Catalog/Catalog.Tests/ProductInStockUpdateEventHandlerTest.cs
+++ Closet.Commerce/src/Services/Catalog/Catalog.Tests/ProductInStockUpdateEventHandlerTest.cs
@@ -5,7 +5,9 @@ using Catalog.Tests.Config;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using static Catalog.Common.Enums;
 
@@ -45,6 +47,102 @@ namespace Catalog.Tests
                         ProductId = productId,
                         Stock = 1,
                         Action = ProductStockAction.Substract
+                    }
+                }
+            }, new CancellationToken()).Wait();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TryToSubstractStockWhenProductHasNotStock()
+        {
+            var context = ApplicationDbContextInMemory.Get();
+            var productInStockId = 2;
+            var productId = 2;
+
+            context.Stocks.Add(new ProductInStock
+            {
+                ProductInStockId = productInStockId,
+                ProductId = productId,
+                Stock = 1,
+            });
+
+            context.SaveChanges();
+            try
+            {
+                var handler = new ProductInStockUpdateEventHandler(context, GetLogger);
+                handler.Handle(new ProductIsStockUpdateStockCommand
+                {
+                    Items = new List<ProductIsStockUpdateItem>()
+                {
+                    new ProductIsStockUpdateItem
+                    {
+                        ProductId = productId,
+                        Stock = 2,
+                        Action = ProductStockAction.Substract
+                    }
+                }
+                }, new CancellationToken()).Wait();
+            }
+            catch (AggregateException ex)
+            {
+                if (ex.GetBaseException() is Exception)
+                {
+                    throw new Exception(ex?.InnerException?.Message);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TryToAddStockWhenProductExists()
+        {
+            var context = ApplicationDbContextInMemory.Get();
+            var productInStockId = 3;
+            var productId = 3;
+
+            context.Stocks.Add(new ProductInStock
+            {
+                ProductInStockId = productInStockId,
+                ProductId = productId,
+                Stock = 1,
+            });
+
+            context.SaveChanges();
+
+            var handler = new ProductInStockUpdateEventHandler(context, GetLogger);
+            handler.Handle(new ProductIsStockUpdateStockCommand
+            {
+                Items = new List<ProductIsStockUpdateItem>()
+                {
+                    new ProductIsStockUpdateItem
+                    {
+                        ProductId = productId,
+                        Stock = 2,
+                        Action = ProductStockAction.Add
+                    }
+                }
+            }, new CancellationToken()).Wait();
+
+            var stockInDB = context.Stocks.Single(x => x.ProductId == productId).Stock;
+            Assert.AreEqual(stockInDB, 3);
+        }
+
+        [TestMethod]
+        public void TryToAddStockWhenProductNotExist()
+        {
+            var context = ApplicationDbContextInMemory.Get();
+            var productId = 4;
+
+            var handler = new ProductInStockUpdateEventHandler(context, GetLogger);
+            handler.Handle(new ProductIsStockUpdateStockCommand
+            {
+                Items = new List<ProductIsStockUpdateItem>()
+                {
+                    new ProductIsStockUpdateItem
+                    {
+                        ProductId = productId,
+                        Stock = 2,
+                        Action = ProductStockAction.Add
                     }
                 }
             }, new CancellationToken()).Wait();
